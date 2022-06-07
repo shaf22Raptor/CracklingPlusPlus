7a8a7cd9ae0c8f2a7baf0b0314b0d079b85795f7
Carl Schmitz <carl.schmitz@connect.qut.edu.au>
2022-06-07 12:00:34 +1000

    Added CHOPCHOP class structure

--- include/CHOPCHOP.hpp
+++ include/CHOPCHOP.hpp
@@ -1,1 +1,20 @@
 // CHOPCHOP.hpp
+#pragma once
+#include <string>
+#include <ConfigManager.hpp>
+#include <Constants.hpp>
+
+class CHOPCHOP
+{
+public:
+	CHOPCHOP(ConfigManager cm);
+
+	void run();
+
+private:
+	int testedcount;
+	int failedcount;
+	bool toolIsSelected;
+
+	bool G20(std::string candidateGuide);
+};
