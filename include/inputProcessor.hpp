31c4e5fd90fe4a3b5ecbb0eb6d8012ca1996c647
Carl Schmitz <carl.schmitz@connect.qut.edu.au>
2022-06-14 14:47:58 +1000

    Added base class for input processor

--- include/inputProcessor.hpp
+++ include/inputProcessor.hpp
@@ -2,10 +2,11 @@
 #pragma once
 #include <list>
 #include <string>
-#include <fstream>
-#include <set>
-#include <regex>
-#include <filesystem>
-#include <Helpers.hpp>
+#include <stdexcept>
 
-std::list<std::string> processInput(std::list<std::string>& filesToProcess);
+class inputProcessor 
+{
+public:
+	virtual std::list<std::string> processInput(std::list<std::string>& filesToProcess, int batchSize);
+
+};
