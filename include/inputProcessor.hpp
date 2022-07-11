462d2429a4910714819122b2b58366f4fab7e61a
Carl Schmitz <carl.schmitz@connect.qut.edu.au>
2022-07-11 15:07:10 +1000

    Formatting inputProcessor

--- include/inputProcessor.hpp
+++ include/inputProcessor.hpp
@@ -13,3 +13,9 @@ public:
 
 	virtual ~inputProcessor() = default;
 };
+
+class NotImplemented : public std::logic_error
+{
+public:
+	NotImplemented() : std::logic_error("Function not yet implemented") { };
+};
--- lib/inputProcessor.cpp
+++ lib/inputProcessor.cpp
@@ -4,12 +4,6 @@ using std::string;
 using std::list;
 using std::logic_error;
 
-class NotImplemented : public std::logic_error
-{
-public:
-	NotImplemented() : std::logic_error("Function not yet implemented") { };
-};
-
 void inputProcessor::process(list<string> const & filesToProcess, int const & batchSize)
 {
 	throw NotImplemented();
