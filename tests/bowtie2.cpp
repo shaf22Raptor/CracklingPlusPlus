c1cd6b8351072d49111fabd19a446847a222492b
Carl Schmitz <carl.schmitz@connect.qut.edu.au>
2022-08-08 11:43:31 +1000

    Added bowtie2 tests

---
+++ tests/bowtie2.cpp
@@ -0,0 +1,55 @@
+#include "../include/bowtie2.hpp"
+#include "../include/doctest.h"
+
+
+TEST_CASE("bowtie2 Module" * doctest::description("Ensure bowtie2 module is working correctly") * doctest::timeout(5))
+{
+    ConfigManager cm("data/test_config.ini");
+    cm.set("general", "optimisation", "ultralow");
+    bowtie2 testModule(cm);
+
+    std::unordered_map<std::string, std::unordered_map<std::string, std::string>> result = {
+        {"ACTCCTCATGCTGGACATTCTGG", {
+            {"bowtieChr"    , CODE_UNTESTED},
+            {"bowtieStart"  , CODE_UNTESTED},
+            { "bowtieEnd"   , CODE_UNTESTED},
+            {"passedBowtie" , CODE_UNTESTED}
+        } },
+        {"ATTCTGGTTCCTAGTATATCTGG", {
+            {"bowtieChr"    , CODE_UNTESTED},
+            {"bowtieStart"  , CODE_UNTESTED},
+            { "bowtieEnd"   , CODE_UNTESTED},
+            {"passedBowtie" , CODE_UNTESTED}
+        } },
+        {"GTATATCTGGAGAGTTAAGATGG", {
+            {"bowtieChr"    , CODE_UNTESTED},
+            {"bowtieStart"  , CODE_UNTESTED},
+            { "bowtieEnd"   , CODE_UNTESTED},
+            {"passedBowtie" , CODE_UNTESTED}
+        }}
+    };
+
+    std::unordered_map<std::string, std::unordered_map<std::string, std::string>> expected = {
+        {"ACTCCTCATGCTGGACATTCTGG", {
+            {"bowtieChr"    , "*"},
+            {"bowtieStart"  , "0"},
+            { "bowtieEnd"   , "22"},
+            {"passedBowtie" , CODE_ACCEPTED}
+        } },
+        {"ATTCTGGTTCCTAGTATATCTGG", {
+            {"bowtieChr"    , "test-genome"},
+            {"bowtieStart"  , "28"},
+            { "bowtieEnd"   , "50"},
+            {"passedBowtie" , CODE_REJECTED}
+        } },
+        {"GTATATCTGGAGAGTTAAGATGG", {
+            {"bowtieChr"    , "*"},
+            {"bowtieStart"  , "0"},
+            { "bowtieEnd"   , "22"},
+            {"passedBowtie" , CODE_ACCEPTED}
+        }}
+    };
+
+    testModule.run(result);
+    CHECK(result == expected);
+}
