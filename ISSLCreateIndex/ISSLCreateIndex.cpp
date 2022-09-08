9b769b89b6a73bdfa2f95ffae0cd67c9203e2403
Carl Schmitz <carl.schmitz@connect.qut.edu.au>
2022-09-08 13:45:41 +1000

    Updated commenting

--- ISSLCreateIndex/ISSLCreateIndex.cpp
+++ ISSLCreateIndex/ISSLCreateIndex.cpp
@@ -9,11 +9,16 @@ using std::regex;
 using std::regex_iterator;
 
 const regex extractNumbers("[1234567890]+");
-
+const vector<uint8_t> nucleotideIndex{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,3 };
+const vector<char> signatureIndex{ 'A', 'C', 'G', 'T' };
 size_t seqLength;
-vector<uint8_t> nucleotideIndex(256);
-vector<char> signatureIndex(4);
 
+/**
+ * getFileSize
+ *
+ * @param path, The path to the file
+ * @return The size of the file (in Bytes) at `path`
+ */
 size_t getFileSize(const char* path)
 {
     struct p_stat64 statBuf;
@@ -21,6 +26,14 @@ size_t getFileSize(const char* path)
     return statBuf.st_size;
 }
 
+/**
+ * sequenceToSignature
+ * 
+ * Converts from sequence of letters to two bit encoded signature
+ * 
+ * @param ptr, A pointer to the beginning of a char seqeuence
+ * @return The two bit encoded signature of the sequence at `ptr`
+ */
 uint64_t sequenceToSignature(const char* ptr)
 {
     uint64_t signature = 0;
@@ -31,6 +44,14 @@ uint64_t sequenceToSignature(const char* ptr)
     return signature;
 }
 
+/**
+ * sequenceToSignature
+ * 
+ * Converts from two bit encoded signature to sequence of letters
+ * 
+ * @param signature, The two bit encoded signature
+ * @return The letter sequence of `signature`
+ */
 string signatureToSequence(uint64_t signature)
 {
     string sequence = string(seqLength, ' ');
@@ -40,34 +61,110 @@ string signatureToSequence(uint64_t signature)
     return sequence;
 }
 
-// combinations
+/**
+ * computeMasksTwoBit
+ *
+ * This function generates all of the combinations of mismatches.
+ *
+ * @param seqLength, The length of the seqeunce 
+ * @param mismatches, The number of mismatches allowed 
+ * @return A vector of combinations
+ */
 vector<uint64_t> computeMasksTwoBit(int seqLength, int mismatches) {
     vector<uint64_t> masks;
 
-    // there are more positions than mismatches
-    if (mismatches < seqLength) {
+    // There will only be one valid combination when mismatches == seqLength.
+    if (mismatches == seqLength) {
 
+        // There are more mismatchs to assign.
         if (mismatches > 0) {
-            // some mismatches across a long sequence
+            /** 
+            * This loop assigns a mismatch to the end of the seqeunce length.
+            * It will then recursively call itself, reducing the seqeunce length and mismatch count.
+            * (1LLU << (seqLength - 1) * 2) is responsible for assigning a mismatch at the end of the current seq length.
+            * By adding the term above to the recursively returned, it will combine them into a unique combination.
+            * E.g 
+            *   Starting with empty seq, seqeunce length 20, mismatch 2: 
+            *   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            * 
+            *   Assign (1LLU << (seqLength - 1) * 2):
+            *   10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            * 
+            *   Recursively call function on remaining seq length, seqeunce length 19, mismatch 1:
+            *   10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            * 
+            *   Input to next call, length 19, mismatch 1:
+            *      00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            * 
+            *   Eventually returned values will be:
+            *      10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            *         10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            *      ....................................................................................................................
+            *                                                                                                                     10 00
+            *                                                                                                                        10
+            *   The loop will add all the recursively genereated values with the original value will give unique combinations:
+            *   10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            * +
+            *      10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            * 
+            *   10 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            * 
+            *   TL:DR This loop reduces both mismatches and total sequence length to generate all combinations from original sequence.
+            */
             for (auto mask : computeMasksTwoBit(seqLength - 1, mismatches - 1)) {
-                // orig: masks.push_back((1 << (seqLength - 1)) + mask);
+
                 masks.push_back((1LLU << (seqLength - 1) * 2) + mask);
             }
+
+            /** 
+            * This loop is run after all combinations are generated with the mismatch base on the first sequence length.
+            * By shrinking the sequence length it will shift the last possible mismatch and generate a new sequence.
+            * This recursive call will result in the process being repeated on this new sequence.
+            * E.g
+            *   Considered the first mask genereated by (1LLU << (seqLength - 1) * 2):
+            *   10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            * 
+            *   By reducing the sequence size the result of (1LLU << (seqLength - 1) * 2) will be:
+            *   00 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            * 
+            *   This sequence will then be used as the starting point for the first loop and the process will repeat:
+            *   (First loop in recursive call)
+            *   Recursively call function on remaining seq length, seqeunce length 19, mismatch 1:
+            *   00 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            *
+            *   Input to next call, length 18, mismatch 1:
+            *   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            *
+            *   Eventually returned values will be:
+            *   00 00 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            *   00 00 00 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            *      ....................................................................................................................
+            *                                                                                                                     10 00
+            *                                                                                                                        10
+            *   The loop will add all the recursively genereated values with the original value will give unique combinations:
+            *   00 00 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            *
+            *   00 00 00 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            *
+            *   00 00 10 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            * 
+            * TL:DR This loop only reduces the sequence length, in order to repeat the first loop process with a new sequence.
+            */
             for (auto mask : computeMasksTwoBit(seqLength - 1, mismatches)) {
                 masks.push_back(mask);
             }
         }
         else {
-            // no mismatches at all
+            // No more mismatches allowed, return a mask of 0 (Exit condition).
             masks.push_back(0LLU);
         }
-
-        // mismatches >= seqLength
-        // every position is going to be a mismatch
-        // eg: mismatches=4 and seqLength=4, we want: 10 10 10 10
     }
+    /**
+    * The only valid combination when mismatches == seqLength will be to assign remaining seq as mismatches.
+    * E.g. mismatches=4 and seqLength=4, we want: 10 10 10 10
+    * Do not recursivley call self as there are no more valid combinations from this point (Exit condition).
+    */ 
     else {
-        // orig: masks.push_back((1LLU << seqLength) - 1);
         uint64_t tempMask = 0;
         for (int i = 0; i < seqLength; i++) {
             tempMask |= (1LLU << i * 2);
@@ -77,9 +174,20 @@ vector<uint64_t> computeMasksTwoBit(int seqLength, int mismatches) {
     return masks;
 }
 
-double single_score(int* mismatch_array, int length) {
+/**
+ * calcMITLocalScore
+ *
+ * This function calculates the local MIT score based on the positions of mismatches.
+ * https://dx.doi.org/10.1038/nbt.2647
+ *
+ * @param mismatch_array, An int array that contains the position of mismatches
+ * @param length, The length of the param `mismatch_array`
+ * @return The local MIT score
+ */
+double calcMITLocalScore(int* mismatch_array, int length) {
     int i;
     double T1 = 1.0, T2, T3, d = 0.0, score;
+    /* Mismatch penalty array */
     double M[] = { 0.0, 0.0, 0.014, 0.0, 0.0, 0.395, 0.317, 0.0, 0.389, 0.079, 0.445, 0.508, 0.613, 0.851, 0.732, 0.828, 0.615, 0.804, 0.685, 0.583 };
 
     /* 1st term */
@@ -99,12 +207,20 @@ double single_score(int* mismatch_array, int length) {
     /* 3rd term */
     T3 = 1.0 / (length * length);
 
-    /* Total score */
+    /* Local score */
     score = T1 * T2 * T3 * 100;
     return score;
 }
 
-double sscore(uint64_t xoredSignatures)
+/**
+ * predictMITLocalScore
+ *
+ * Calculates mistmatch postions before calling scoring equation.
+ *
+ * @param xoredSignatures, A mask that represents the mismatches between a potential target and off-target. (The result of the XOR operation in practice)
+ * @return The local MIT score
+ */
+double predictMITLocalScore(uint64_t xoredSignatures)
 {
     int mismatch_array[20], m = 0;
     for (size_t j = 0; j < seqLength; j++) {
@@ -113,11 +229,12 @@ double sscore(uint64_t xoredSignatures)
         }
     }
     if (m == 0) return 0.0;
-    return single_score(mismatch_array, m);
+    return calcMITLocalScore(mismatch_array, m);
 }
 
 int main(int argc, char** argv)
 {
+    // Check input args
     if (argc < 5) {
         fprintf(stderr, "Usage: %s [offtargetSites.txt] [sequence length] [slice width (bits)] [sissltable]\n", argv[0]);
         exit(1);
@@ -154,7 +271,7 @@ int main(int argc, char** argv)
     if (sliceRanges.size() < 2) {
         fprintf(stderr, "Error: Please specific more than 2 slice lengths\n");
         fprintf(stderr, "Please format slice length list like [(start:end)...(start:end)]\n");
-        exit(1);
+        exit(1); 
     }
     vector<size_t> sliceLens;
     for (int i = 0; i < sliceRanges.size(); i = i + 2) {
@@ -164,22 +281,12 @@ int main(int argc, char** argv)
     size_t seqCount = fileSize / seqLineLength;
     fprintf(stderr, "Number of sequences: %zu\n", seqCount);
 
-
-    nucleotideIndex['A'] = 0;
-    nucleotideIndex['C'] = 1;
-    nucleotideIndex['G'] = 2;
-    nucleotideIndex['T'] = 3;
-    signatureIndex[0] = 'A';
-    signatureIndex[1] = 'C';
-    signatureIndex[2] = 'G';
-    signatureIndex[3] = 'T';
-
     size_t globalCount = 0;
 
     vector<uint64_t> seqSignatures;
     vector<uint32_t> seqSignaturesOccurrences;
 
-    size_t distinctSites = 0;
+    size_t offtargetsCount = 0;
     {
         vector<char> entireDataSet(fileSize);
 
@@ -202,42 +309,47 @@ int main(int argc, char** argv)
             while (memcmp(ptr, ptr + (seqLineLength * occurrences), seqLength) == 0) {
                 occurrences++;
                 if ((seqCount - progressCount - occurrences) < 100)
-                    fprintf(stderr, "%zu/%zu : %zu\n", (progressCount + occurrences), seqCount, distinctSites);
+                    fprintf(stderr, "%zu/%zu : %zu\n", (progressCount + occurrences), seqCount, offtargetsCount);
 
             }
 
             seqSignatures.push_back(signature);
             seqSignaturesOccurrences.push_back(occurrences);
 
-            distinctSites++;
+            offtargetsCount++;
             if (progressCount % 10000 == 0)
-                fprintf(stderr, "%zu/%zu : %zu\n", progressCount, seqCount, distinctSites);
+                fprintf(stderr, "%zu/%zu : %zu\n", progressCount, seqCount, offtargetsCount);
 
             progressCount += occurrences;
         }
 
     }
     printf("Finished counting occurrences, now constructing index...\n");
-    size_t offtargetsCount = distinctSites;
 
     vector<vector<vector<uint64_t>>> sliceLists(sliceLens.size());
+    // Assign sliceLists size based on each slice length
     for (int i = 0; i < sliceLens.size(); i++)
     {
-        sliceLists[i] = vector<vector<uint64_t>>(1 << sliceLens[i]);
+        sliceLists[i] = vector<vector<uint64_t>>(1ULL << sliceLens[i]);
     }
-    /*#pragma omp parallel for*/
-    for (int i = 0; i < sliceLens.size(); i++) {
-        uint64_t sliceMask = (1 << sliceLens[i]) - 1;
-        int sliceShift = sliceRanges[i * 2] - 2;
-        sliceMask = sliceMask << sliceShift;
-        auto& sliceList = sliceLists[i];
 
+    // Generate ISSL Index
+    #pragma omp parallel for
+    for (int i = 0; i < sliceLens.size(); i++) {
+        // sliceMask is a mask of 1's the length of the slice, used to extract target slice
+        uint64_t sliceMask = (1 << sliceLens[i]) - 1;
+        // sliceShift is the amout the target signature needs to be shifted to align with the target slice position 
+        int sliceShift = sliceRanges[i * 2] - 2;
+        auto& sliceList = sliceLists[i];
+        
         uint32_t signatureId = 0;
         for (uint64_t signature : seqSignatures) {
             uint32_t occurrences = seqSignaturesOccurrences[signatureId];
-            uint64_t test = signature & sliceMask;
-            uint8_t sliceVal = (signature & sliceMask) >> sliceShift;
-
+            // Shift the target signature to align with target slice position 
+            uint8_t sliceVal = (signature >> sliceShift) & sliceMask;
+            // seqSigIdVal represnets the sequence signature ID and number of occurrences of the associated sequence.
+            // (((uint64_t)occurrences) << 32), the most significant 32 bits is the count of the occurrences.
+            // (uint64_t)signatureId, the index of the sequence in `seqSignatures`
             uint64_t seqSigIdVal = (((uint64_t)occurrences) << 32) | (uint64_t)signatureId;
             sliceList[sliceVal].push_back(seqSigIdVal);
             signatureId++;
@@ -256,7 +368,7 @@ int main(int argc, char** argv)
         vector<uint64_t> tempMasks;
         tempMasks = computeMasksTwoBit(20, i);
         for (auto mask : tempMasks) {
-            double score = sscore(mask);
+            double score = predictMITLocalScore(mask);
             precalculatedScores.insert(pair<uint64_t, double>(mask, score));
             scoresCount++;
         }
@@ -265,14 +377,15 @@ int main(int argc, char** argv)
     printf("Finished calculating scores, now preparing to write to disk...\n");
 
     fp = fopen(argv[4], "wb");
+    // Generate Header list
     vector<size_t> slicelistHeader;
     slicelistHeader.push_back(offtargetsCount);
     slicelistHeader.push_back(seqLength);
     slicelistHeader.push_back(seqCount);
-    slicelistHeader.push_back(sliceLens.size());
+    slicelistHeader.push_back(sliceRanges.size());
     slicelistHeader.push_back(scoresCount);
    
-    // write the header
+    // write the header, reserve the first 50 for header information
     fwrite(slicelistHeader.data(), sizeof(size_t), 50, fp);
 
     // write slice ranges
@@ -290,15 +403,17 @@ int main(int argc, char** argv)
     // write the offtargets
     fwrite(seqSignatures.data(), sizeof(uint64_t), seqSignatures.size(), fp);
 
+    // write slice list lengths
     for (size_t i = 0; i < sliceLens.size(); i++) { // Number of slices
-        for (size_t j = 0; j < (1 << sliceLens[i]); j++) { // Slice limit give slice width
+        for (size_t j = 0; j < (1ULL << sliceLens[i]); j++) { // Slice limit given slice width
             size_t sz = sliceLists[i][j].size();
             fwrite(&sz, sizeof(size_t), 1, fp);
         }
     }
 
+    // write slice list data
     for (size_t i = 0; i < sliceLens.size(); i++) { // Number of slices
-        for (size_t j = 0; j < (1 << sliceLens[i]); j++) { // Slice limit give slice width
+        for (size_t j = 0; j < (1ULL << sliceLens[i]); j++) { // Slice limit given slice width
             fwrite(sliceLists[i][j].data(), sizeof(uint64_t), sliceLists[i][j].size(), fp); // vector
         }
     }
