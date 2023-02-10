bc835baafc5d357b6432568d48bc0951a6560693
Carl Schmitz <carl.schmitz@connect.qut.edu.au>
2023-02-10 15:03:58 +1000

    Implemented chopchop module

---
+++ include/chopchopModule.hpp
@@ -0,0 +1,23 @@
+#ifndef chopchopModuleInclude
+#define chopchopModuleInclude
+
+#include <iostream>
+#include <string>
+#include <string_view>
+#include "../include/util.hpp"
+#include "../include/consensusModule.hpp"
+#define FMT_HEADER_ONLY
+#include "../include/fmt/format.h"
+
+class chopchopModule : private consensusModule
+{
+public:
+	chopchopModule(cracklingConfig config);
+	void run(std::vector<guideResults>& candidateGuides) final;
+private:
+	bool toolIsSelected;
+	bool G20(std::string_view guide);
+	bool processGuide(const guideResults& guide) final;
+};
+
+#endif // !chopchopModuleInclude
---
+++ lib/chopchopModule.cpp
@@ -0,0 +1,65 @@
+#include "../include/chopchopModule.hpp"
+
+using std::string_view;
+using std::cout;
+using std::endl;
+
+chopchopModule::chopchopModule(cracklingConfig config) : consensusModule(config)
+{
+	this->toolIsSelected = config.consensus.chopchop;
+}
+
+void chopchopModule::run(std::vector<guideResults>& candidateGuides)
+{
+    if (!toolIsSelected)
+    {
+        cout << "CHOPCHOP has been configured not to run. Skipping CHOPCHOP" << endl;
+        return;
+    }
+
+
+    cout << "CHOPCHOP - remove those without G in position 20." << endl;
+    uint64_t failedCount = 0;
+    uint64_t testedCount = 0;
+    for (guideResults& candidate : candidateGuides)
+    {
+        // Run time filtering
+        if (!processGuide(candidate)) { continue; }
+
+        if (G20(candidate.seq))
+        {
+            candidate.passedG20 = CODE_ACCEPTED;
+        }
+        else
+        {
+            candidate.passedG20 = CODE_REJECTED;
+            failedCount++;
+        }
+        testedCount++;
+    }
+
+    cout << fmt::format("\t{} of {} failed here.", failedCount, testedCount) << endl;
+    return;
+}
+
+
+bool chopchopModule::processGuide(const guideResults& guide)
+{
+    // Process all guides at this level
+    if (this->optimsationLevel == optimisationLevel::ultralow) { return true; }
+
+    // For all levels above `ultralow`
+    if (!guide.isUnique)
+    {
+        // Reject all guides that have been seen more than once
+        return false;
+    }
+
+    // None of the failure conditions have been meet, return true
+    return true;
+}
+
+bool chopchopModule::G20(string_view guide)
+{
+	return guide[19] == 'G';
+}
