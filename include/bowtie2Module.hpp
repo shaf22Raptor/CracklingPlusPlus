be5bdfaa2a20ec262687d3654efc357fb55b7911
Carl Schmitz <carl.schmitz@connect.qut.edu.au>
2023-02-24 14:40:19 +1000

    Added bowtie2 module

---
+++ include/bowtie2Module.hpp
@@ -0,0 +1,24 @@
+#ifndef bowtie2ModuleInclude
+#define bowtie2ModuleInclude
+#include <string>
+#include <vector>
+#include <fstream>
+#include <filesystem>
+#include <unordered_map>
+#include <boost/algorithm/string.hpp>
+#include "../include/specificityModule.hpp"
+#include "../include/util.hpp"
+
+class bowtie2Module : private specificityModule
+{
+public:
+	bowtie2Module(cracklingConfig config);
+	void run(std::vector<guideResults>& candidateGuides) final;
+private:
+	bowtie2Config config;
+	std::filesystem::path input;
+	uint8_t consensusN;
+	bool processGuide(const guideResults& guide) final;
+};
+
+#endif // !bowtie2ModuleInclude
